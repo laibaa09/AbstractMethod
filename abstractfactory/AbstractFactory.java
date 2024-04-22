daf16a2b491a3751779e6ac6c292bfdc9c58a127
laibaa09 <115113457+laibaa09@users.noreply.github.com>
2024-04-22 13:20:22 +0500

    Update AbstractFactory.java

--- abstractfactory/AbstractFactory.java
+++ abstractfactory/AbstractFactory.java
@@ -15,7 +15,34 @@ public class AbstractFactory {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+          AbstractFactory shapeFactory = FactoryProducer.getFactory(false);
+      //get an object of Shape Rectangle
+      Shape shape1 = shapeFactory.getShape("RECTANGLE");
+      //call draw method of Shape Rectangle
+      shape1.draw();
+      //get an object of Shape Square 
+      Shape shape2 = shapeFactory.getShape("SQUARE");
+      //call draw method of Shape Square
+      shape2.draw();
+      //get shape factory
+        Shape shape3 = shapeFactory.getShape("TRIANGLE");
+      //call draw method of Shape Square
+      shape3.draw();
+      //get shape factory
+      AbstractFactory shapeFactory1 = FactoryProducer.getFactory(true);
+      //get an object of Shape Rectangle
+      Shape shape3 = shapeFactory1.getShape("RECTANGLE");
+      //call draw method of Shape Rectangle
+      shape3.draw();
+      //get an object of Shape Square 
+      Shape shape4 = shapeFactory1.getShape("SQUARE");
+      //call draw method of Shape Square
+      shape4.draw();
+       Shape shape5 = shapeFactory1.getShape("TRIANGLE");
+      //call draw method of Shape Rectangle
+      shape3.draw();
+      
+   }
     }
     
 }
