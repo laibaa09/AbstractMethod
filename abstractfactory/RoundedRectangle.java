3898cfbc01b7220da7e63bcec13e7c79b1bf8675
laibaa09 <115113457+laibaa09@users.noreply.github.com>
2024-04-22 13:24:42 +0500

    RoundedRectangle.java

--- abstractfactory/RoundedRectangle.java
+++ abstractfactory/RoundedRectangle.java
@@ -10,5 +10,7 @@ package abstractfactory;
  * @author sp21-bse-041
  */
 public class RoundedRectangle {
-    
+     public void draw() {
+      System.out.println("Inside RoundedRectangle::draw() method.");
+   }
 }
