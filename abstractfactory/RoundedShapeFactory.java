e368ef251ff82fa05ccc34b3d669e09d004a1984
laibaa09 <115113457+laibaa09@users.noreply.github.com>
2024-04-22 13:26:19 +0500

    RoundedShapeFactory.java

--- abstractfactory/RoundedShapeFactory.java
+++ abstractfactory/RoundedShapeFactory.java
@@ -10,13 +10,16 @@ package abstractfactory;
  * @author sp21-bse-041
  */
 public class RoundedShapeFactory {
-      public RoundedRectangle getShape(String shapeType){    
+         public RoundedRectangle getShape(String shapeType){    
       if(shapeType.equalsIgnoreCase("RECTANGLE")){
          return new RoundedRectangle();         
       }
       else if(shapeType.equalsIgnoreCase("SQUARE")){
          return new RoundedSquare();
       }	 
+      else if(shapeType.equalsIgnoreCase("TRIANGLR")){
+         return new RoundedTriangle();
+      }	
       return null;
    }
 }
